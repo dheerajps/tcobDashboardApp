87d97eebc07e9998a55d927dd96cd3693139305b
unknown <cwm262@cob-mdc4.col.missouri.edu>
2015-09-28 16:04:15 -0500

    Adding database sql files for reference

---
+++ Database Scripts/dashboardProcedures.sql
@@ -0,0 +1,28 @@
+/* FIRST PROCEDURE */
+
+USE [mubusassessment]
+GO
+/****** Object:  StoredProcedure [dbo].[getURLbyGroupIdAndSection]    Script Date: 9/28/2015 3:26:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		cwm
+-- Create date: 9-28-2015
+-- Description:	Query DB for URL_Name and URL_Address for a specific Group_ID
+-- =============================================
+ALTER PROCEDURE [dbo].[getURLbyGroupIdAndSection]
+	@groupid varchar(30),
+	@section varchar(50)
+AS
+	SELECT dashboard_urls.url_name, dashboard_urls.url_address FROM dashboard_urls
+	INNER JOIN dashboard_urls_by_group
+		ON dashboard_urls.url_name = dashboard_urls_by_group.url_name JOIN dashboard_groups
+		ON dashboard_urls_by_group.group_id = dashboard_groups.group_id
+	WHERE dashboard_groups.group_id = @groupid AND dashboard_urls.url_section = @section
+	ORDER BY dashboard_urls.url_name ASC;
+
+	/**************************/
+
+	
---
+++ Database Scripts/dashboardTables.sql
@@ -0,0 +1,40 @@
+-- TABLE STRUCTURES
+
+-- Stores the groups
+CREATE TABLE dashboard_groups(
+	group_id VARCHAR(30) PRIMARY KEY,
+	-- Current list:
+	--	acct
+	--	adm
+	--	adv
+	--	busCarServ
+	--	crosby
+	--	dev
+	--	execComm
+	--	execMba
+	--	finance
+	--	gradProg
+	--	market
+	--	mgmt
+	--	phd
+	--	profDev
+	--	stAbroad
+	--	ugProg
+	group_name VARCHAR(50) NOT NULL
+	-- Elaborates on the ID. e.g., busCarServ = Business Career Services
+);
+
+-- Stores the URLs
+CREATE TABLE dashboard_urls (
+	url_name VARCHAR(75) PRIMARY KEY, -- Page Title, e.g., Advising Dashboard or Business Metrics Dashboard or etc
+	url_address VARCHAR(MAX) NOT NULL, -- Hyperlink
+	url_section VARCHAR(50) NOT NULL -- To which section does this URL belong (i.e., There are TOPICS (e.g., Strategic Initiative) with SECTIONS under them)
+);
+
+-- Handles many to many relationship between dashboard_groups and dashboard_urls
+CREATE TABLE dashboard_urls_by_group(
+	url_name VARCHAR(75) NOT NULL, -- See dashboard_urls
+	group_id VARCHAR(30) NOT NULL, -- see dashboard_groups
+	FOREIGN KEY (url_name) REFERENCES dashboard_urls,
+	FOREIGN KEY (group_id) REFERENCES dashboard_groups
+);
