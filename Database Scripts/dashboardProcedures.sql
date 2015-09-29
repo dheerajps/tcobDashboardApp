927f1d3a3bb8d942fb9da5862d505b05311aa4a8
Srivatsav, Dheeraj P. (MU-Student) <dpstm3@mail.missouri.edu>
2015-09-29 16:01:04 -0500

    The new stored procedure to retrieve url_names and url_address based on groupid

--- Database Scripts/dashboardProcedures.sql
+++ Database Scripts/dashboardProcedures.sql
@@ -13,16 +13,13 @@ GO
 -- Description:	Query DB for URL_Name and URL_Address for a specific Group_ID
 -- =============================================
 ALTER PROCEDURE [dbo].[getURLbyGroupIdAndSection]
-	@groupid varchar(30),
-	@section varchar(50)
+	@groupid varchar(30)
 AS
-	SELECT dashboard_urls.url_name, dashboard_urls.url_address FROM dashboard_urls
+	SELECT dashboard_urls.url_name, dashboard_urls.url_address, dashboard_urls.section_id, dashboard_urls.topic_id FROM dashboard_urls
 	INNER JOIN dashboard_urls_by_group
 		ON dashboard_urls.url_name = dashboard_urls_by_group.url_name JOIN dashboard_groups
 		ON dashboard_urls_by_group.group_id = dashboard_groups.group_id
-	WHERE dashboard_groups.group_id = @groupid AND dashboard_urls.url_section = @section
+	WHERE dashboard_groups.group_id = @groupid
 	ORDER BY dashboard_urls.url_name ASC;
 
 	/**************************/
-
-	
