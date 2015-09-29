1d4c16b2b9dea8d026e8ea562e385490e18cfa33
Srivatsav, Dheeraj P. (MU-Student) <dpstm3@mail.missouri.edu>
2015-09-29 15:53:04 -0500

    the changes made to the database are now consistent here

--- Database Scripts/dashboardTables.sql
+++ Database Scripts/dashboardTables.sql
@@ -28,7 +28,10 @@ CREATE TABLE dashboard_groups(
 CREATE TABLE dashboard_urls (
 	url_name VARCHAR(75) PRIMARY KEY, -- Page Title, e.g., Advising Dashboard or Business Metrics Dashboard or etc
 	url_address VARCHAR(MAX) NOT NULL, -- Hyperlink
-	url_section VARCHAR(50) NOT NULL -- To which section does this URL belong (i.e., There are TOPICS (e.g., Strategic Initiative) with SECTIONS under them)
+	section_id VARCHAR(50) NULL, -- To which section does this URL belong (i.e., There are TOPICS (e.g., Strategic Initiative) with SECTIONS under them)
+	topic_id VARCHAR(50) NULL, -- Topic it belongs to 
+	FOREIGN KEY(section_id) REFERENCES dashboard_topics,
+	FOREIGN KEY(topic_id) REFERENCES dashboard_topics
 );
 
 -- Handles many to many relationship between dashboard_groups and dashboard_urls
@@ -38,3 +41,22 @@ CREATE TABLE dashboard_urls_by_group(
 	FOREIGN KEY (url_name) REFERENCES dashboard_urls,
 	FOREIGN KEY (group_id) REFERENCES dashboard_groups
 );
+
+
+CREATE TABLE dashboard_sections(
+section_id VARCHAR(50) PRIMARY KEY, 
+--	a unique section id
+section_name VARCHAR(50) NOT NULL
+-- not sure about the names the sections can get so it has section1 section2, section3...
+);
+
+CREATE TABLE dashboard_topics(
+topic_id VARCHAR(50) PRIMARY KEY, 
+--	a unique id for each topic
+topic_name VARCHAR(50) NOT NULL
+-- not sure about the names the sections can get so it has section1 section2, section3...
+);
+
+
+
+
