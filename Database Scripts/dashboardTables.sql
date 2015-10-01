d5d318f73b79fe42c6afdbfd4994f56c95e22f51
cwm262 <azathoth.hp@gmail.com>
2015-10-01 11:56:41 -0500

    Updated database scripts, including table creation & stored procedures

--- Database Scripts/dashboardTables.sql
+++ Database Scripts/dashboardTables.sql
@@ -1,62 +1,72 @@
 -- TABLE STRUCTURES
 
--- Stores the groups
-CREATE TABLE dashboard_groups(
+-- Creates dashboard_groups
+-- group_id		group_name
+-- 
+-- acc			ACCOUNTANCY
+-- adm			ADMINSTRATOR
+-- adv			ADVISING
+-- bus			BUSINESS CAREER SERVICES
+-- cros			CROBY MBA
+-- dev			DEVELOPMENT
+-- exec			EXECUTIVE COMMITTE
+-- execMBA		EXEC MBA
+-- fin			FINANCE
+-- grad			GRADUATE PROGAMS
+-- mgmt			MANAGEMENT
+-- phd			PHD
+-- ugp			UG PROG				
+CREATE TABLE dbo.dashboard_groups(
 	group_id VARCHAR(30) PRIMARY KEY,
-	-- Current list:
-	--	acct
-	--	adm
-	--	adv
-	--	busCarServ
-	--	crosby
-	--	dev
-	--	execComm
-	--	execMba
-	--	finance
-	--	gradProg
-	--	market
-	--	mgmt
-	--	phd
-	--	profDev
-	--	stAbroad
-	--	ugProg
 	group_name VARCHAR(50) NOT NULL
-	-- Elaborates on the ID. e.g., busCarServ = Business Career Services
 );
 
--- Stores the URLs
-CREATE TABLE dashboard_urls (
-	url_name VARCHAR(75) PRIMARY KEY, -- Page Title, e.g., Advising Dashboard or Business Metrics Dashboard or etc
-	url_address VARCHAR(MAX) NOT NULL, -- Hyperlink
-	section_id VARCHAR(50) NULL, -- To which section does this URL belong (i.e., There are TOPICS (e.g., Strategic Initiative) with SECTIONS under them)
-	topic_id VARCHAR(50) NULL, -- Topic it belongs to 
-	FOREIGN KEY(section_id) REFERENCES dashboard_topics,
-	FOREIGN KEY(topic_id) REFERENCES dashboard_topics
+-- Creates dashboard topics
+-- topic_id		topic_name
+-- BUS			Business Things
+-- IMP			Impact Measures
+-- OTH			Other Things
+-- STR			Strategic Initiatives
+CREATE TABLE dbo.dashboard_topics(
+	topic_id VARCHAR(30) PRIMARY KEY,
+	topic_name VARCHAR(50) NOT NULL
 );
 
--- Handles many to many relationship between dashboard_groups and dashboard_urls
-CREATE TABLE dashboard_urls_by_group(
-	url_name VARCHAR(75) NOT NULL, -- See dashboard_urls
-	group_id VARCHAR(30) NOT NULL, -- see dashboard_groups
-	FOREIGN KEY (url_name) REFERENCES dashboard_urls,
-	FOREIGN KEY (group_id) REFERENCES dashboard_groups
+-- Creates dashboard sections table
+-- 	section_id	section_name
+--	SEC1		Section 1
+--	SEC2		Section 2
+--	SEC3		Section 3
+CREATE TABLE dbo.dashboard_sections(
+	section_id VARCHAR(30) PRIMARY KEY,
+	section_name VARCHAR(50) NOT NULL
 );
 
-
-CREATE TABLE dashboard_sections(
-section_id VARCHAR(50) PRIMARY KEY, 
---	a unique section id
-section_name VARCHAR(50) NOT NULL
--- not sure about the names the sections can get so it has section1 section2, section3...
+-- Creates URL table
+--bingDashboard		BING	http://www.bing.com/
+--fbDashboard		FACEBOOK	https://www.facebook.com/
+--googleDashboard	GOOGLE 	https://www.google.com/
+--mizzouDashboard	MIZZOU	http://missouri.edu/
+--twDashboard		TWITTER	https://twitter.com/
+CREATE TABLE dbo.dashboard_urls(
+	url_id VARCHAR(30) PRIMARY KEY,
+	url_name VARCHAR(50) NOT NULL,
+	url_address VARCHAR(MAX) NOT NULL
 );
 
-CREATE TABLE dashboard_topics(
-topic_id VARCHAR(50) PRIMARY KEY, 
---	a unique id for each topic
-topic_name VARCHAR(50) NOT NULL
--- not sure about the names the sections can get so it has section1 section2, section3...
+CREATE TABLE dbo.dashboard_sections_by_topics(
+	section_id VARCHAR(30) REFERENCES dbo.dashboard_sections,
+	topic_id VARCHAR(30) REFERENCES dbo.dashboard_topics
 );
 
+CREATE TABLE dbo.dashboard_url_by_section(
+	url_id VARCHAR(30) REFERENCES dbo.dashboard_urls,
+	section_id VARCHAR(30) REFERENCES dbo.dashboard_sections
+);
 
+CREATE TABLE dbo.dashboard_topics_by_group(
+	topic_id VARCHAR(30) REFERENCES dbo.dashboard_topics,
+	group_id VARCHAR(30) REFERENCES dbo.dashboard_groups
+);
 
 
