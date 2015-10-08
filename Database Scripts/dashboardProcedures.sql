9b6f42406b3980e7a55514baf6ad205470c36941
Srivatsav, Dheeraj P. (MU-Student) <dpstm3@mail.missouri.edu>
2015-10-08 14:07:35 -0500

    made small changes to the stored procedure. This gives out data somewhat in a manner close to what Aditya/Thomas want.

--- Database Scripts/dashboardProcedures.sql
+++ Database Scripts/dashboardProcedures.sql
@@ -1,3 +1,6 @@
+USE [mubusassessment]
+GO
+/****** Object:  StoredProcedure [dbo].[getURLs]    Script Date: 10/8/2015 2:05:14 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -7,14 +10,15 @@ GO
 -- Create date: 10-8-2015
 -- Description:	Return all URLs and their respective sections and topics by taking in a group_id as a parameter
 -- =============================================
-CREATE PROCEDURE dbo.getURLs
+ALTER PROCEDURE [dbo].[getURLs]
 	@groupID VARCHAR(50)
 AS
 BEGIN
-	SELECT * FROM dbo.dashboard_urls INNER JOIN dbo.dashboard_linkURLtoGroup
+	SELECT dashboard_topics.topic_name, dashboard_sections.section_name,dashboard_urls.url_name, dashboard_urls.url_address FROM dbo.dashboard_urls INNER JOIN dbo.dashboard_linkURLtoGroup
 		ON dbo.dashboard_urls.url_id = dbo.dashboard_linkURLtoGroup.url_id JOIN dbo.dashboard_groups
-		ON dbo.dashboard_linkURLtoGroup.group_id = dbo.dashboard_groups.group_id
+		ON dbo.dashboard_linkURLtoGroup.group_id = dbo.dashboard_groups.group_id JOIN dbo.dashboard_sections
+		on dbo.dashboard_urls.section_id = dbo.dashboard_sections.section_id JOIN dbo.dashboard_topics
+		on dbo.dashboard_urls.topic_id = dbo.dashboard_topics.topic_id
 	WHERE dbo.dashboard_groups.group_id = @groupID
 	ORDER BY dbo.dashboard_urls.url_name ASC;
 END
-GO
