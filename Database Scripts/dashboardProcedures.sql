fd1fe8379f546aff318b711ecd391d6c243f01d6
cwm262 <azathoth.hp@gmail.com>
2015-10-08 11:17:50 -0500

    Updating database script files

--- Database Scripts/dashboardProcedures.sql
+++ Database Scripts/dashboardProcedures.sql
@@ -1,87 +1,20 @@
-USE [mubusassessment]
-GO
-/****** Object:  StoredProcedure [dbo].[getTopicInfoByGroupId]    Script Date: 10/1/2015 10:53:56 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
--- Author:		CWM / DPS
--- Create date: 10-1-2015
--- Description:	Takes group_id as parameter
---				Returns the topics assigned to that group_id in ascending order by topic_name 
+-- Author:		cwm
+-- Create date: 10-8-2015
+-- Description:	Return all URLs and their respective sections and topics by taking in a group_id as a parameter
 -- =============================================
-ALTER PROCEDURE [dbo].[getTopicInfoByGroupId]
-	@groupid VARCHAR(30)
+CREATE PROCEDURE dbo.getURLs
+	@groupID VARCHAR(50)
 AS
 BEGIN
-	SELECT dashboard_topics.topic_id, dashboard_topics.topic_name FROM dashboard_topics
-	INNER JOIN dashboard_topics_by_group
-		ON dashboard_topics.topic_id = dashboard_topics_by_group.topic_id JOIN dashboard_groups
-		ON dashboard_topics_by_group.group_id = dashboard_groups.group_id
-	WHERE dashboard_groups.group_id = @groupid
-	ORDER BY dashboard_topics.topic_name ASC;
-END
-
--- *****************************************************************
-
-USE [mubusassessment]
-GO
-/****** Object:  StoredProcedure [dbo].[getTopicInfoByGroupId]    Script Date: 10/1/2015 11:36:27 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
--- =============================================
--- Author:		CWM / DPS
--- Create date: 10-1-2015
--- Description:	Takes group_id as parameter
---				Returns the topics assigned to that group_id in ascending order by topic_name 
--- =============================================
-ALTER PROCEDURE [dbo].[getTopicInfoByGroupId]
-	@groupid VARCHAR(30)
-AS
-BEGIN
-	SELECT dashboard_topics.topic_id, dashboard_topics.topic_name FROM dashboard_topics
-	INNER JOIN dashboard_topics_by_group
-		ON dashboard_topics.topic_id = dashboard_topics_by_group.topic_id JOIN dashboard_groups
-		ON dashboard_topics_by_group.group_id = dashboard_groups.group_id
-	WHERE dashboard_groups.group_id = @groupid
-	ORDER BY dashboard_topics.topic_name ASC;
-END
-
--- *********************************************************************
-
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
--- =============================================
--- Author:		cwm / dps
--- Create date: 10-1-2015
--- Description:	Takes in section_id as parameter
---				Returns url_id, url_name, url_address ORDER BY url_name ASCENDING for that section
--- =============================================
-CREATE PROCEDURE [dbo].[getURLinfoBySectionId]
-	@section_id VARCHAR(30)
-AS
-BEGIN
-	SELECT dashboard_urls.url_id, dashboard_urls.url_name, dashboard_urls.url_address FROM dashboard_urls
-	INNER JOIN dashboard_url_by_section
-		ON dashboard_urls.url_id = dashboard_url_by_section.url_id JOIN dashboard_sections
-		ON dashboard_sections.section_id = dashboard_url_by_section.section_id
-	WHERE dashboard_sections.section_id = @section_id
-	ORDER BY dashboard_urls.url_name ASC;
+	SELECT * FROM dbo.dashboard_urls INNER JOIN dbo.dashboard_linkURLtoGroup
+		ON dbo.dashboard_urls.url_id = dbo.dashboard_linkURLtoGroup.url_id JOIN dbo.dashboard_groups
+		ON dbo.dashboard_linkURLtoGroup.group_id = dbo.dashboard_groups.group_id
+	WHERE dbo.dashboard_groups.group_id = @groupID
+	ORDER BY dbo.dashboard_urls.url_name ASC;
 END
 GO
--- Database Scripts/dashboardTables.sql
+++ Database Scripts/dashboardTables.sql
@@ -1,72 +1,29 @@
--- TABLE STRUCTURES
-
--- Creates dashboard_groups
--- group_id		group_name
--- 
--- acc			ACCOUNTANCY
--- adm			ADMINSTRATOR
--- adv			ADVISING
--- bus			BUSINESS CAREER SERVICES
--- cros			CROBY MBA
--- dev			DEVELOPMENT
--- exec			EXECUTIVE COMMITTE
--- execMBA		EXEC MBA
--- fin			FINANCE
--- grad			GRADUATE PROGAMS
--- mgmt			MANAGEMENT
--- phd			PHD
--- ugp			UG PROG				
-CREATE TABLE dbo.dashboard_groups(
-	group_id VARCHAR(30) PRIMARY KEY,
-	group_name VARCHAR(50) NOT NULL
+CREATE TABLE dbo.dashboard_groups (
+	group_id VARCHAR(50) PRIMARY KEY
 );
 
--- Creates dashboard topics
--- topic_id		topic_name
--- BUS			Business Things
--- IMP			Impact Measures
--- OTH			Other Things
--- STR			Strategic Initiatives
-CREATE TABLE dbo.dashboard_topics(
-	topic_id VARCHAR(30) PRIMARY KEY,
-	topic_name VARCHAR(50) NOT NULL
+CREATE TABLE dbo.dashboard_topics (
+	topic_id VARCHAR(50) PRIMARY KEY,
+	topic_name VARCHAR(MAX) NOT NULL
 );
 
--- Creates dashboard sections table
--- 	section_id	section_name
---	SEC1		Section 1
---	SEC2		Section 2
---	SEC3		Section 3
-CREATE TABLE dbo.dashboard_sections(
-	section_id VARCHAR(30) PRIMARY KEY,
-	section_name VARCHAR(50) NOT NULL
+CREATE TABLE dbo.dashboard_sections (
+	section_id VARCHAR(50) PRIMARY KEY,
+	section_name VARCHAR(MAX) NOT NULL,
 );
 
--- Creates URL table
---bingDashboard		BING	http://www.bing.com/
---fbDashboard		FACEBOOK	https://www.facebook.com/
---googleDashboard	GOOGLE 	https://www.google.com/
---mizzouDashboard	MIZZOU	http://missouri.edu/
---twDashboard		TWITTER	https://twitter.com/
-CREATE TABLE dbo.dashboard_urls(
-	url_id VARCHAR(30) PRIMARY KEY,
-	url_name VARCHAR(50) NOT NULL,
-	url_address VARCHAR(MAX) NOT NULL
+CREATE TABLE dbo.dashboard_urls (
+	url_id VARCHAR(50) PRIMARY KEY,
+	url_name VARCHAR(MAX) NOT NULL,
+	topic_id VARCHAR(50),
+	section_id VARCHAR(50),
+	FOREIGN KEY (topic_id) REFERENCES dbo.dashboard_topics,
+	FOREIGN KEY (section_id) REFERENCES dbo.dashboard_sections
 );
 
-CREATE TABLE dbo.dashboard_sections_by_topics(
-	section_id VARCHAR(30) REFERENCES dbo.dashboard_sections,
-	topic_id VARCHAR(30) REFERENCES dbo.dashboard_topics
+CREATE TABLE dbo.dashboard_linkURLtoGroup (
+	url_id VARCHAR(50),
+	group_id VARCHAR(50),
+	FOREIGN KEY (url_id) REFERENCES dbo.dashboard_urls,
+	FOREIGN KEY (group_id) REFERENCES dbo.dashboard_groups
 );
-
-CREATE TABLE dbo.dashboard_url_by_section(
-	url_id VARCHAR(30) REFERENCES dbo.dashboard_urls,
-	section_id VARCHAR(30) REFERENCES dbo.dashboard_sections
-);
-
-CREATE TABLE dbo.dashboard_topics_by_group(
-	topic_id VARCHAR(30) REFERENCES dbo.dashboard_topics,
-	group_id VARCHAR(30) REFERENCES dbo.dashboard_groups
-);
-
-
