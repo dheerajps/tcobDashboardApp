81548ac027a246919219be5746679c468a910864
cwm262 <azathoth.hp@gmail.com>
2015-10-22 10:39:34 -0500

    Added stored procedures to query db. Added authentication logic to DatabaseHelper/LDAP.php

--- Database Scripts/dashboardProcedures.sql
+++ Database Scripts/dashboardProcedures.sql
@@ -1,24 +1,25 @@
 USE [mubusassessment]
 GO
-/****** Object:  StoredProcedure [dbo].[getURLs]    Script Date: 10/8/2015 2:05:14 PM ******/
+
+/****** Object:  StoredProcedure [dbo].[getDashboardData]    Script Date: 10/20/2015 12:35:03 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		cwm
--- Create date: 10-8-2015
--- Description:	Return all URLs and their respective sections and topics by taking in a group_id as a parameter
+-- Create date: 10-14-2015
+-- Description:	get ALL the data with group_id as a parameter
 -- =============================================
-ALTER PROCEDURE [dbo].[getURLs]
-	@groupID VARCHAR(50)
+CREATE PROCEDURE [dbo].[getDashboardData] 
+	@groupid VARCHAR(MAX)
 AS
 BEGIN
-	SELECT dashboard_topics.topic_name, dashboard_sections.section_name,dashboard_urls.url_name, dashboard_urls.url_address FROM dbo.dashboard_urls INNER JOIN dbo.dashboard_linkURLtoGroup
-		ON dbo.dashboard_urls.url_id = dbo.dashboard_linkURLtoGroup.url_id JOIN dbo.dashboard_groups
-		ON dbo.dashboard_linkURLtoGroup.group_id = dbo.dashboard_groups.group_id JOIN dbo.dashboard_sections
-		on dbo.dashboard_urls.section_id = dbo.dashboard_sections.section_id JOIN dbo.dashboard_topics
-		on dbo.dashboard_urls.topic_id = dbo.dashboard_topics.topic_id
-	WHERE dbo.dashboard_groups.group_id = @groupID
-	ORDER BY dbo.dashboard_urls.url_name ASC;
+	declare @sql nvarchar(MAX)
+	SET @sql = 'SELECT DISTINCT dashboard_topics.topic_name, dashboard_sections.section_name,dashboard_urls.url_name, dashboard_urls.url_address FROM dbo.dashboard_urls INNER JOIN dbo.dashboard_linkURLtoGroup ON dbo.dashboard_urls.url_id = dbo.dashboard_linkURLtoGroup.url_id JOIN dbo.dashboard_groups ON dbo.dashboard_linkURLtoGroup.group_id = dbo.dashboard_groups.group_id JOIN dbo.dashboard_sections ON dbo.dashboard_urls.section_id = dbo.dashboard_sections.section_id JOIN dbo.dashboard_topics ON dbo.dashboard_urls.topic_id = dbo.dashboard_topics.topic_id WHERE dbo.dashboard_groups.group_id IN (' + @groupid + ')'
+	EXECUTE sp_executesql @sql
 END
+
+GO
